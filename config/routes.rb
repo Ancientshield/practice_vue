1c9c2e552327991dde0865e2393c9fb0fbda5d57
Ancientsword <jacklin2100@gmail.com>
2020-05-18 18:14:16 +0800

    add vuetify

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  root to: 'welcome#index'
+  resources :users
+  get 'app', to: 'users#index'
 end
