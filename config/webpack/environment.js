f9cd3df6551240f2dcf8438c67aa0903c75d0407
Ancientsword <jacklin2100@gmail.com>
2020-05-20 00:33:00 +0800

    restore heroku css in vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,6 +4,34 @@ const vue = require("./loaders/vue");
 
 environment.plugins.prepend("VueLoaderPlugin", new VueLoaderPlugin());
 environment.loaders.prepend("vue", vue);
+environment.exports = {
+  rules: [
+    {
+      test: /\.s(c|a)ss$/,
+      use: [
+        "vue-style-loader",
+        "css-loader",
+        {
+          loader: "sass-loader",
+          // Requires sass-loader@^7.0.0
+          options: {
+            implementation: require("sass"),
+            fiber: require("fibers"),
+            indentedSyntax: true, // optional
+          },
+          // Requires sass-loader@^8.0.0
+          options: {
+            implementation: require("sass"),
+            sassOptions: {
+              fiber: require("fibers"),
+              indentedSyntax: true, // optional
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
 
 const resolver = {
   resolve: {
@@ -12,6 +40,5 @@ const resolver = {
     },
   },
 };
-environment.config.merge(resolver);
 
 module.exports = environment;
