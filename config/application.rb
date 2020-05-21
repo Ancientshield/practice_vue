e18db5ea9d913517874f9cf0a5892b44a3daa909
Ancientsword <jacklin2100@gmail.com>
2020-05-21 20:19:24 +0800

    remove assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module PracticeVue
     config.load_defaults 6.0
     config.assets.compile = false
     config.serve_static_assets = false
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
