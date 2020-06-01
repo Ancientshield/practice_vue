df9bbd7b67cb7d183ead1d669c72833b22938ead
Ancientsword <jacklin2100@gmail.com>
2020-06-02 01:26:51 +0800

    remove surplus app.vue

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -1,34 +1,26 @@
 module.exports = {
-  rules: [
-    {
-      test: /\.s(c|a)ss$/,
-      use: [
-        "vue-style-loader",
-        "css-loader",
-        {
-          loader: "sass-loader",
-          // Requires sass-loader@^7.0.0
-          options: {
-            implementation: require("sass"),
+  rules: {
+    test: /\.s(c|a)ss$/,
+    use: [
+      "vue-style-loader",
+      "css-loader",
+      {
+        loader: "sass-loader",
+        // Requires sass-loader@^7.0.0
+        options: {
+          implementation: require("sass"),
+          fiber: require("fibers"),
+          indentedSyntax: true, // optional
+        },
+        // Requires sass-loader@^8.0.0
+        options: {
+          implementation: require("sass"),
+          sassOptions: {
             fiber: require("fibers"),
             indentedSyntax: true, // optional
           },
-          // Requires sass-loader@^8.0.0
-          options: {
-            implementation: require("sass"),
-            sassOptions: {
-              fiber: require("fibers"),
-              indentedSyntax: true, // optional
-            },
-          },
         },
-      ],
-    },
-  ],
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.js",
-      vue_resource: "vue-resource/dist/vue-resource",
-    },
+      },
+    ],
   },
 };
