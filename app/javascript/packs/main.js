54e25b9c576000202be54cc5ef00566c036f50f3
Ancientsword <jacklin2100@gmail.com>
2020-06-04 06:40:16 +0800

    註解require(@rails/ujs).start();

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import Vuetify from "vuetify";
-import user from "./components/User";
+import user from "./components/user.vue";
 
 Vue.use(Vuetify);
 
@@ -8,6 +8,6 @@ document.addEventListener("turbolinks:load", () => {
   const app = new Vue({
     el: '[data-behavior="vue-app"]',
     vuetify: new Vuetify(),
-    components: { user },
+    components: user,
   });
 });
